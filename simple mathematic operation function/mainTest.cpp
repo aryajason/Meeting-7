373e3f8a04dbc50a38b421dd7af7a90cee2947ce
aryajason <aryajasonr@gmail.com>
2024-10-28 13:37:52 +0700

    Update mainTest.cpp

--- simple mathematic operation function/mainTest.cpp
+++ simple mathematic operation function/mainTest.cpp
@@ -1,1 +1,26 @@
-testing
+#include <iostream>
+using namespace std;
+
+// Function to perform addition
+void add(int a, int b) {
+    int result = a + b;
+    cout << "The sum is " << result << endl;
+}
+
+// Function to perform subtraction
+void subtract(int a, int b) {
+    int result = a - b;
+    cout << "The difference between " << a << " and " << b << " is: " << result << endl;
+}
+
+int main() {
+  int a = 10
+  int b = 5
+
+  double expectedAdd = 55;
+	double expectedDiff = 35;
+
+	if(expectedAdd==add(a, b) && expectedDiff==subtract(a, b))
+	{
+		cout<<"SUCCESSFULLY"<<'\n';
+	}
