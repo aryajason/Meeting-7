5aea0ca31b9faf1d83c3992cb193249fb06cb62d
aryajason <aryajasonr@gmail.com>
2024-10-30 11:30:33 +0700

    Create mainTest.cpp

---
+++ X factorial function/mainTest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int x) {
+    if (x <= 1) { 
+    return 1;
+    } 
+    else {
+        return x * factorial(x - 1); 
+    }
+}
+int main() {
+    int num = 5;
+
+    int expOutput = 120;
+
+	if(expOutput==fac(x)) cout<<"SUCCESSFULLY"<<'\n';
+	else cout<<"FAILED!!!"<<'\n';
+
+    return 0;
+}
